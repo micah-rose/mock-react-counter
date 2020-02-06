6e60b19130d1fec779f1ba4c948ad8eb1e2b1ec3
micah-rose <micah.r.lund@gmail.com>
2020-02-06 13:56:41 -0700

    Finalized the CurrentDate component and determined that formatting needs to be done when calling the component on App.js.

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default class App extends Component {
     return(
       <div>
         <div className="date">
-          <Date />
+          <Date format="MMMM D, YYYY"/>
         </div>
         <div className="counter">
           <h2>Travelex Count:</h2>
--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -1,19 +1,12 @@
 import React, {Component} from "react";
-import DateTime from "react-datetime";
 import Moment from 'react-moment';
- 
+
 class CurrentDate extends Component {
-  state = {
-    todayDate: new Date().getDate()
-  };
- 
 
   render() {
     return (
       <div class="date">
-        <Moment>
-          {this.state.todayDate}
-        </Moment>
+        <Moment />
       </div>
     );
   }
